382a79c0502a37bc151203f4821532aa28281740
moeinheydaridev <moeinheydaridev@gmail.com>
2024-08-04 12:24:43 +0330

    Add files via upload

---
+++ ExpectionHandler/ExpectionHandler/Expecton.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+
+namespace ExpectionHandler
+{ public class Expecton
+    {
+        int[] array = { 1, 34, 21, 45, 43 };
+        public static bool WrongAnswer = false;
+        public int GetNumber(int index)
+        {
+            int output = -1;
+            try
+            {
+                output = array[index];
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+}
+            finally
+            {
+                if (output == -1)
+                {
+                    WrongAnswer = true;
+                }
+                else
+                {
+                    WrongAnswer = false;
+
+                }
+            }
+            return output;
+        }
+    }
+}
+
---
+++ ExpectionHandler/ExpectionHandler/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExpectionHandler
+{
+
+    public class Program
+    {
+static void Main(string[] args)
+        {
+            Console.WriteLine("please insert an index");
+
+            var index = Convert.ToInt32(Console.ReadLine());
+
+            var exc = new Expecton();
+
+            var result = exc.GetNumber(index);
+            if (Expecton.WrongAnswer)
+            {
+                Console.WriteLine("the number is wrong");
+            }
+            else
+            {
+
+                Console.WriteLine($"the number is {result}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
